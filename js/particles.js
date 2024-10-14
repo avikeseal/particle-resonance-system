c3d6860863fa871e11400d0535225ad5121d82aa
Avike Seal <avikeseal@gmail.com>
2024-10-14 13:11:30 -0400

    basic environment setup

--- js/particles.js
+++ js/particles.js
@@ -1,18 +1,17 @@
-//canvas element where we will draw our WebGl content
-const canvas = document.getElementById('particleCanvas');
+//basic setup
+//creates a new scene 
+const scene = new THREE.Scene();
+//creates a PerspectiveCamera which mimics how human eye percieve depth:
+//75 argument sets the field of view in degrees
+//second argument defines the aspect ratio (screen width divided by height)
+//0.1 and 1000 define the near and far clipping planes. Objects closer than 0.1
+//units or farther than 1000 units from the camera won't be rendered
+const camera = new THREE.PerspectiveCamera(
+    75, window.innerWidth / window.innerHeight, 0.1, 1000);
+//handles the actual drawing of the scene onto the screen using WebGL
+const renderer = new THREE.WebGLRenderer();
+//set renderer size where it fills the entire screen
+renderer.setSize(window.innerWidth, window.innerHeight);
+//adds the renderer's canvas to the webpage so it appears on the screen
+document.body.appendChild(renderer.domElement);
 
-//getting WebGl context from the canvas
-//this is what allows us to draw using WebGl functions
-let gl = canvas.getContext('webgl');
-
-//checking if WebGl is supported by thr browser
-//if not, fallback to experimental-webgl, which is a backward-compatible version
-if (!gl) {
-    console.log('WebGl not supported, falling back on experimental-webgl');
-    gl = canvas.getContext('experimental-webgl');
-}
-
-//if neither WebGl nor experimental-webgl is available, alert the user 
-if (!gl) {
-    alert('Your browser does not support WebGl');
-}
