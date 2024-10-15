26d28a2d8ce1c43629b237f3193c586a2fa21b5d
Avike Seal <avikeseal@gmail.com>
2024-10-15 14:27:03 -0400

    basic scene setup

--- js/particles.js
+++ js/particles.js
@@ -1,1 +1,11 @@
+//basic setup:
+const scene = new THREE.Scene();
+//set the background color to cream white:
+scene.background = new THREE.Color(0xfcfbf4);
 
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth
+    / window.innerHeight, 0.1, 1000
+);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appenChild(renderer.domElement);
