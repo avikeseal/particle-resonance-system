30d7dc888555a52d13310a927e6d4b12c52ee47a
Avike Seal <avikeseal@gmail.com>
2024-10-16 13:39:34 -0400

    rendering two basic particles

--- js/particles.js
+++ js/particles.js
@@ -1,50 +1,53 @@
 //basic setup:
 const scene = new THREE.Scene();
 //set the background color to cream white:
-scene.background = new THREE.Color(0xfcfbf4);
+//scene.background = new THREE.Color(0xfcfbf4);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth
-    / window.innerHeight, 0.1, 1000
-);
+    / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appenChild(renderer.domElement);
+document.body.appendChild(renderer.domElement);
 
-//adding black particles:
-//particle size:
-const geometry = new THREE.SphereGeometry(0.2, 32, 32)
-//color:
-const material = new THREE.MeshBasicMaterial({color: 0x000000});
-const particle1 = new THREE.Mesh(geometry, material);
-const particle = new THREE.Mesh(geometry, material);
 
-scene.add(particle1);
-scene.add(particle2);
+//particle system setup with just two partcles:
+const particleCount = 2;
+const particles = new THREE.BufferGeometry();
+const positions = new Float32Array(particleCount * 3);
 
-//camera position:
+//manually position two particles in 3D space:
+positions[0] = -1; //x of particle 1
+positions[1] = 0; //y of particle 1
+positions[2] = 0; //z of particle 1
+
+positions[0] = -1; //x of particle 2
+positions[0] = -1; //y of particle 2
+positions[0] = -1; //z of particle 2
+
+particle.setAttribute('position', new THREE.BUfferAttribute(poisitions, 3));
+
+
+//particle material:
+const particleMaterial = new THREE.PointsMaterial({
+    color: 0xffffff, //white color
+    size: 0.1 //size 
+});
+
+//creating particle system:
+const particleSystem = new THREE.Points(particles, particleMaterial);
+scene.add(particleSystem);
+
+//set camera position:
 camera.position.z = 5;
 
-//animation loop:
-let angle = 0;
-const radius = 2; //orbit radius
-
+//rendering loop:
 function animate() {
     requestAnimationFrame(animate);
-
-    //update angle
-    angle += 0.02;
-
-    //particle 1 moves clockwise:
-    particle1.position.x = radius * Math.cos(angle);
-    particle1.position.y = radius * Math.sin(angle);
-
-    //particle 2 moves counterclockwise:
-    particle2.position.x = radius * Math.cos(-angle);
-    particle2.position.y = radius * Math.sin(-angle);
 
     renderer.render(scene, camera);
 }
 
 animate();
+
 
 
